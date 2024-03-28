237ba0853a1a86b5618fc0d965ca8ce089d75ae0
Bluegnarl <bluegnarl@gmail.com>
2024-03-28 07:51:45 +0100

    vercel fix

--- public/index.php
+++ public/index.php
@@ -1,15 +1,32 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/styles/style.css">
-    <title>EasyStudy - Koçak Ali</title>
-</head>
-<body>
-    <h1>Choose Translation Direction</h1>
-    <a href="/frtoen.php">French to English</a>
-    <a href="/entofr.php">English to French</a>
-    <footer>Par <b>Koçak Ali</b> ou <b>Bluegnarl</b></footer>
-</body>
-</html>
+<?php
+
+function home(){
+    require_once __DIR__ . "/home.php";
+}
+
+function frtoen(){
+    require_once __DIR__ . "/frtoen.php";
+}
+
+function entofr(){
+    require_once __DIR__ . "/entofr.php";
+}
+
+$page = $_GET['page'] ?? null;
+
+switch ($page) {
+    case 'home':
+        home();
+    break;
+    case 'frtoen':
+        frtoen();
+    break;
+    case 'entofr':
+        entofr();
+        break;
+    default:
+        home();
+        break;
+}
+
+?>
