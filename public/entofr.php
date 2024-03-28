a067073a79cc5a8dceca93b68322457193babc9a
Bluegnarl <bluegnarl@gmail.com>
2024-03-28 06:52:07 +0100

    vercel.json fix

--- public/entofr.php
+++ public/entofr.php
@@ -29,7 +29,7 @@ $response = $_GET['response'] ?? null;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="/styles/style.css">
     <title>EasyStudy</title>
 </head>
 <body>
