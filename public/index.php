5318ff802b2ccf484477fc49800201b926ceb290
Bluegnarl <bluegnarl@gmail.com>
2024-05-20 23:21:57 +0200

    greedy test

--- public/index.php
+++ public/index.php
@@ -1,32 +1,52 @@
 <?php
+session_start();
 
-function home(){
-    require_once __DIR__ . "/home.php";
+$state = $_GET['state'] ?? null;
+
+function nextphp() {
+    $study = json_decode(file_get_contents(__DIR__ . '/datas/bricks.json'));
+    $study_length = count($study);
+    $study_rand = rand(0, $study_length - 1);
+    $study_local = array(
+        "french" => $study[$study_rand]->french,
+        "english" => $study[$study_rand]->english
+    );
+
+    $_SESSION['study_local'] = $study_local;
 }
 
-function frtoen(){
-    require_once __DIR__ . "/frtoen.php";
+nextphp();
+
+if ($state == 'next') {
+    nextphp();
+    header("Location: /");
+    exit();
 }
 
-function entofr(){
-    require_once __DIR__ . "/entofr.php";
-}
-
-$page = $_GET['page'] ?? null;
-
-switch ($page) {
-    case 'home':
-        home();
-    break;
-    case 'frtoen':
-        frtoen();
-    break;
-    case 'entofr':
-        entofr();
-        break;
-    default:
-        home();
-        break;
-}
+$response = $_GET['response'] ?? null;
 
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/style.css">
+    <title>EasyStudy</title>
+</head>
+<body>
+<a href="/" style="position: absolute; top: 0; margin: 24px; width: auto; font-weight: 600;">Main Menu</a>
+<span>Don't use <b>capital letters</b> and don't forget <b>"to" before an infinitive</b></span>
+<?php if ($state == 'response' && $response !== null) : ?>
+    <p class="<?= ($response == $_SESSION['study_local']['french']) ? "correct" : "incorrect" ?>">Your response "<?= $response ?>" is <?= ($response == $_SESSION['study_local']['french']) ? "correct" : "incorrect" ?></p>
+<?php endif ?>
+<form action="/" method="get">
+    <p><h3> English :</h3> <?= isset($_SESSION['study_local']['english']) ? $_SESSION['study_local']['english'] : '' ?></p>
+    <input type="text" name="response" value="<?= $response ?>" placeholder="Response In French">
+    <input type="hidden" name="state" value="response">
+    <button type="submit">Send</button>
+</form>
+<a href="/?state=next">Randomize</a> <!-- Lien pour générer une nouvelle question -->
+<footer>Par <b>Koçak Ali</b> ou <b>Bluegnarl</b></footer>
+</body>
+</html>
